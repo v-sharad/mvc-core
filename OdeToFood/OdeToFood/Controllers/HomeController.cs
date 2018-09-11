ba6c3d7d54e0e4d1d0c0bc47d3019f7045e9e615
Sharad Venkataraman <v.sharad@outlook.com>
2018-09-11 17:55:54 +0530

    Module 5.1 - Introducing ViewModel

--- OdeToFood/OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OdeToFood.Models;
 using OdeToFood.Services;
+using OdeToFood.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +12,19 @@ namespace OdeToFood.Controllers
     public class HomeController : Controller
     {
         private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData, IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
 
         public IActionResult Index()
         {
-            var model = _restaurantData.GetAll();
+            var model = new HomeIndexViewModel();
+            model.Restaurants = _restaurantData.GetAll();
+            model.CurrentMessage = _greeter.GetMessageOfTheDay();
             return View(model);
         }
     }
