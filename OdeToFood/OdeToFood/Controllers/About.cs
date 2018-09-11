b0cfb8c53af6327770a292518151928aba3ef41e
Sharad Venkataraman <v.sharad@outlook.com>
2018-09-11 16:00:47 +0530

    Module 4.2 - Implemented Attribute based routing

---
+++ OdeToFood/OdeToFood/Controllers/About.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers
+{
+    [Route("company/[controller]/[action]")]
+    public class AboutController
+    {
+        [Route("secret")]
+        public string Phone()
+        {
+            return "+91 9876543210"; // accessible by going to url - company/about/phone/secret
+        }
+        public string Address()
+        {
+            return "+91 9876543210";
+        }
+    }
+}
