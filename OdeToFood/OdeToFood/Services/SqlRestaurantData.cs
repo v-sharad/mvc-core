367df22640196f721038749220675785fbf13f3a
Sharad Venkataraman <v.sharad@outlook.com>
2018-09-12 12:32:26 +0530

    Module 6 - 1. Implementation of DbContext 2. Configuration of application and 3. Using migrations to create database schema

---
+++ OdeToFood/OdeToFood/Services/SqlRestaurantData.cs
@@ -0,0 +1,36 @@
+﻿using OdeToFood.Data;
+using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private OdeToFoodDbContext _context;
+
+        public SqlRestaurantData(OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+        public Restaurant Add(Restaurant restaurant)
+        {
+            _context.Restaurants.Add(restaurant); // creates the id column in database server
+            _context.SaveChanges(); 
+            return restaurant; // the created id column is passed on to the object
+        }
+
+        public Restaurant Get(int Id)
+        {
+            return _context.Restaurants.FirstOrDefault(r => r.Id == Id);            
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _context.Restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
