04367d5ae76a82d12aac67e06445c8c6a9f1fcbe
Sharad Venkataraman <v.sharad@outlook.com>
2018-09-12 16:05:34 +0530

    Module 7.3 - Creating Edit Page

--- OdeToFood/OdeToFood/Services/InMemoryRestaurantData.cs
+++ OdeToFood/OdeToFood/Services/InMemoryRestaurantData.cs
@@ -6,37 +6,37 @@ using System.Threading.Tasks;
 
 namespace OdeToFood.Services
 {
-    public class InMemoryRestaurantData : IRestaurantData
-    {
-        public InMemoryRestaurantData()
-        {
-            _restaurants = new List<Restaurant>
-            {
-                new Restaurant { Id=1,Name="Pizza Place"  },
-                new Restaurant { Id=2,Name="Burger Place"  },
-                new Restaurant { Id=3,Name="Sandwich Place"  }
-        };
-        }  
+    //public class InMemoryRestaurantData : IRestaurantData
+    //{
+    //    public InMemoryRestaurantData()
+    //    {
+    //        _restaurants = new List<Restaurant>
+    //        {
+    //            new Restaurant { Id=1,Name="Pizza Place"  },
+    //            new Restaurant { Id=2,Name="Burger Place"  },
+    //            new Restaurant { Id=3,Name="Sandwich Place"  }
+    //    };
+    //    }  
 
-        public IEnumerable<Restaurant> GetAll()
-        {
-            return _restaurants.OrderBy(r => r.Name);
-        }
+    //    public IEnumerable<Restaurant> GetAll()
+    //    {
+    //        return _restaurants.OrderBy(r => r.Name);
+    //    }
 
-        public Restaurant Get(int Id)
-        {
-            return _restaurants.FirstOrDefault(r => r.Id == Id);
-        }
+    //    public Restaurant Get(int Id)
+    //    {
+    //        return _restaurants.FirstOrDefault(r => r.Id == Id);
+    //    }
 
-        public Restaurant Add(Restaurant restaurant)
-        {
-            restaurant.Id = _restaurants.Max(r => r.Id) + 1;
-            _restaurants.Add(restaurant);
-            return restaurant;
-        }
+    //    public Restaurant Add(Restaurant restaurant)
+    //    {
+    //        restaurant.Id = _restaurants.Max(r => r.Id) + 1;
+    //        _restaurants.Add(restaurant);
+    //        return restaurant;
+    //    }
 
-        List<Restaurant> _restaurants;
-    }
+    //    List<Restaurant> _restaurants;
+    //}
     
 
 }
