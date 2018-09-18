23fd781b01f9cd94302c713fb3bd092880bcbb30
Sharad Venkataraman <v.sharad@outlook.com>
2018-09-18 19:13:12 +0530

    Module 9 - Finished designing

--- OdeToFood/OdeToFood/Startup.cs
+++ OdeToFood/OdeToFood/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -15,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using OdeToFood.Data;
 using OdeToFood.Services;
+using System.Threading.Tasks;
+
 namespace OdeToFood
 {
     public class Startup
@@ -37,6 +35,17 @@ namespace OdeToFood
             .AddOpenIdConnect(options =>
             {
                 _configuration.Bind("GoogleOpenId", options);
+                options.Events = new OpenIdConnectEvents()
+                {
+                    // handle the logout redirection 
+                    OnRedirectToIdentityProviderForSignOut = context =>
+                    {
+                        var logoutUri = "/Account/SignedOut";
+                        context.Response.Redirect(logoutUri);
+                        context.HandleResponse();
+                        return Task.CompletedTask;
+                    }
+                };
             })
             .AddCookie();
             services.AddSingleton<IGreeter, Greeter>();
@@ -57,47 +66,14 @@ namespace OdeToFood
             app.UseRewriter(new RewriteOptions()
                                 .AddRedirectToHttpsPermanent());
 
-            // Function that will be invoked only once when .net core framework is setting up the pipeline
-            app.Use(next =>
-                {
-                    #region middleware function that is invoked per Http request
-                    return async context =>
-                    {
-                        logger.LogInformation("Request incoming");
-                        if (context.Request.Path.StartsWithSegments("/mym"))
-                        {
-                            await context.Response.WriteAsync("My Middleware responding!!!");
-                            logger.LogInformation("Request handled");
-                        }
-                        else
-                        {
-                            await next(context);
-                            logger.LogInformation("Response outgoing");
-                        }
-                    };
-                    #endregion
-                });
-
-            //app.UseDefaultFiles(); //Use index.html as default page
+            
             app.UseStaticFiles();// Use static files like .js .html 
+
+            app.UseNodeModules(env.ContentRootPath);
 
             app.UseAuthentication();
 
-            app.UseMvc(ConfigureRoutes);
-
-            app.UseWelcomePage(
-                new WelcomePageOptions
-                {
-                    Path="/wp"
-                }
-                );
-
-            app.Run(async (context) =>
-            {
-                //throw new Exception("Exception");
-                string greeting = greeter.GetMessageOfTheDay();
-                await context.Response.WriteAsync($"{greeting} : {env.EnvironmentName}");
-            });
+            app.UseMvc(ConfigureRoutes);  
         }
 
         private void ConfigureRoutes(IRouteBuilder routeBuilder)
